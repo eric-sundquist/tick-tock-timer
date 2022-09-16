366cfc80c07c6a5a223484959889d8a86a57aa72
Eric Sundqvist <es225kz@student.lnu.se>
2022-09-16 16:30:53 +0200

    Add more logic to Timer, test in node

--- timer-lib/Timer.js
+++ timer-lib/Timer.js
@@ -1,9 +1,37 @@
 export class Timer {
-  #message
-  constructor() {
-    this.#message = 'hello world'
+  #startTime
+  
+  #currentTime
+  
+  #expireTime
+  
+  #isRunning = false
+  
+  #isPaused = false
+
+  /**
+   * 
+   * @param {Number} expireTime - Seconds until timer should expire/ring. 
+   */
+  constructor(setTimer) {
+    this.#expireTime = setTimer * 1000
+    this.#currentTime = setTimer
   }
-  get message() {
-    return this.#message
+
+  /**
+   * @returns {Number} - current time of timer in seconds.
+   */
+  get time() {
+    const timePassedSinceStart = Date.now() - this.#startTime
+    this.#currentTime = this.#expireTime - timePassedSinceStart
+    // return Math.round(this.#currentTime / 1000)
+    return this.#currentTime
+  }
+
+  start() {
+    if (this.#isRunning) return
+
+    this.#startTime = Date.now()
+    this.#isRunning = true
   }
 }
